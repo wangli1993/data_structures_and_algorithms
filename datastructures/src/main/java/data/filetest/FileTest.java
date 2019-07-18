8492ebb61d60a2024426844c8fe592a505ea99ef
wangli1993 <13721410471@163.com>
2019-07-18 23:00:22 +0800

    线程join问题

--- datastructures/src/main/java/data/filetest/FileTest.java
+++ datastructures/src/main/java/data/filetest/FileTest.java
@@ -1,4 +1,25 @@
 package data.filetest;
 
 public class FileTest {
+    public static boolean initFlag = false;
+
+    static {
+        System.out.println("***");
+        Thread t = new Thread(() -> initFlag = true);
+        System.out.println("111");
+        t.start();
+        System.out.println("222");
+        System.out.println(initFlag);
+        System.out.println(11);
+        try {
+            t.join();
+            System.out.println("333");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(initFlag);
+    }
 }
